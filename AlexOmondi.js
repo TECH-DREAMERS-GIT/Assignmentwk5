bbca589ce85f15f703e81a76fb82bf39f88d27f8
Alex Omondi Otieno <123232209+TECH-DREAMERS-GIT@users.noreply.github.com>
2024-03-25 22:50:07 +0300

    Add files via upload

---
+++ AlexOmondi.js
@@ -0,0 +1,13 @@
+function addNumbers(num1, num2) {
+    2    // Add num1 and num2
+    3    const sum = num1 + num2;
+    4
+    5    // Return the result of the addition
+    6    return sum;
+    7}
+    8
+    9// Test cases
+    10console.log(addNumbers(3, 5)); // Expected output: 8
+    11console.log(addNumbers(-1, 7)); // Expected output: 6
+    12console.log(addNumbers(0, 0)); // Expected output: 0
+    
